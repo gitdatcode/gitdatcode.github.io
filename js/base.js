48063fcca2c0e184f388b715b0704f9e787e933e
Jer'Maine Jones <jermaine.m.jones@outlook.com>
2017-09-25 17:13:49 -0500

    landing page 2.0!

---
+++ js/base.js
@@ -0,0 +1,30 @@
+let sa = require("superagent");
+let vars = require("./vars");
+
+$(document).ready( () => {
+  
+  //create object for POST to dobot
+  let dobot = {
+    'event': {
+      'type': "signup"
+    }
+  };
+
+  let subscribe_button = document.getElementById("mc-embedded-subscribe");
+
+  subscribe_button.addEventListener( 'click', () => {
+    //get the dobot values from the form
+    dobot.event.first_name = document.getElementById("mce-FNAME").value;
+    dobot.event.last_name = document.getElementById("mce-LNAME").value;
+    dobot.event.email = document.getElementById("mce-EMAIL").value;
+
+    //POST to the dobot app
+    sa.post('http://45.55.90.231:8921')
+      .set('Content-Type', 'application/json')
+      .send(JSON.stringify(dobot))
+      .end( (err, res) => {
+        console.log('error', err);
+        console.log(res);
+    });
+  });
+});
--- scripts/vars.js
+++
@@ -1,5 +0,0 @@
-const vars = {
-  "SLACK_API_TOKEN": "exqoE99l8QOZ6T1wq6uEaprO"
-}
-
-module.exports = vars;
